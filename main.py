779404a1642f77bac6c305c7da87ea8cee565709
djmoore926 <djmoore926@gmail.com>
2025-08-27 01:51:45 +0000

    project finished... I think...

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 from stats import *
+import sys
 
 def main():
-    text = get_book_text(r"books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
+    text = get_book_text(path)
     num_words = get_book_words(text)
-    print(f"{num_words} words found in the document")
-    print(get_book_chars(text))
+    stats = format_char_num(get_book_chars(text))
+    # print(f"{num_words} words found in the document")
+    # print(get_book_chars(text))
+    stats.sort(key=get_num, reverse=True)
+    trimmed_stats = trim_stats(stats)
+    print(f"""
+===============BOOKBOT===============
+Analyzing book found at {path}...
+------------ Word Count -------------
+Found {num_words} total words
+---------- Character Count ----------""")
+    pull_values(trimmed_stats)
+    print("=================END================="
+)
+
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
+    
+def pull_values(stats):
+    for item in stats:
+        print(f'{item["char"]}: {item["num"]}')
 
 if __name__ == "__main__":
 
--- stats.py
+++ stats.py
@@ -9,3 +9,19 @@ def get_book_chars(string):
         else:
             chars[i] = 1
     return chars
+
+def format_char_num(stats):
+    formatted = []
+    for k,v in stats.items():
+        formatted.append({"char": k, "num": v})
+    return formatted
+
+def get_num(pair):
+    return pair["num"]
+
+def trim_stats(stats):
+    trimmed = []
+    for pair in stats:
+        if pair["char"].isalpha():
+            trimmed.append(pair)
+    return trimmed
